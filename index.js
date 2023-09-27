3ba824c1333281a316039d29a723b7668abbd86b
Ted Castelijns <ted.castelijns@gmail.com>
2023-09-27 08:09:33 +0200

    log requests

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
+  console.log(`${Date.now()} - Hello world from Ted`);
   res.send('Hello world from Ted\n');
 });
 
